0fba0aa21c986bc68397e116ef992dcf1e3bc6aa
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2025-04-14 19:22:20 -0300

    lista

--- index.js
+++ index.js
@@ -47,4 +47,19 @@ botaoAdicionar.addEventListener("click", (evento) => {
   itemDaLista.appendChild(itemData);
 
   listaDeCompras.appendChild(itemDaLista);
+
+  verificaListaVazia();
 });
+
+const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+function verificaListaVazia() {
+  const itensDaLista = listaDeCompras.querySelectorAll("li");
+  if (itensDaLista.length === 0) {
+    mensagemListaVazia.style.display = "block";
+  } else {
+    mensagemListaVazia.style.display = "none";
+  }
+}
+
+verificaListaVazia();
