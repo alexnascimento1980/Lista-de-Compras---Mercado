33c902c84c48e7e226be6ae8a3b447d0db7baca4
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2025-04-14 22:08:34 -0300

    aula 5

--- scripts/gerarDiaDaSemana.js
+++ scripts/gerarDiaDaSemana.js
@@ -1,16 +1,15 @@
 function gerarDiaDaSemana() {
   const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
-    weekday: "long",
+      weekday: "long"
   });
-  const data = new Date().toLocaleDateString("pt-BR");
+  const data = new Date().toLocaleDateString("pt-BR")
   const hora = new Date().toLocaleTimeString("pt-BR", {
-    hour: "numeric",
-    minute: "numeric",
-  });
+      hour: "numeric",
+      minute: "numeric"
+  })
+  const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
 
-  const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
-
-  return dataCompleta;
+  return dataCompleta
 }
 
 export default gerarDiaDaSemana;
