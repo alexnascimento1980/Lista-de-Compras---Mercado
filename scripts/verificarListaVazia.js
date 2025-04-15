15e5408c5cedb3e2e9aee8bce686bf98739fb7cd
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2025-04-14 22:19:35 -0300

    aula 5.2

--- scripts/verificarListaVazia.js
+++ scripts/verificarListaVazia.js
@@ -1,6 +1,6 @@
 const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
 
-function verificarListaVazia(listaDeCompras) {
+export function verificarListaVazia(listaDeCompras) {
   const itensDaLista = listaDeCompras.querySelectorAll("li");
   if (itensDaLista.length === 0) {
     mensagemListaVazia.style.display = "block";
